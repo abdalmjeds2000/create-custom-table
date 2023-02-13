df8abcd7d3457aec3042de6004826c345be70546
abdalmjeds2000 <94318021+abdalmjeds2000@users.noreply.github.com>
2023-02-13 23:48:49 +0200

    1 file modified

--- src/SortableTable.jsx
+++ src/SortableTable.jsx
@@ -4,7 +4,7 @@ import Table from './Table.jsx';
 const Table = (props) => {
   const [sortOrder, setSortOrder] = useState(null);
   const [sortBy, setSortBy] = useState(null);
-  const { config } = props;
+  const { config, data } = props;
 
   const handleClick = (label) => {
     alert(label);
@@ -27,12 +27,41 @@ const Table = (props) => {
     return {
       ...column,
       header: (
-        <th onClick={() => handleClick(column.label)}>{column.label} SORT</th>
+        <th onClick={() => handleClick(column.label)}>
+          {getIcon(column.label, sortBy, sortOrder)}
+          {column.label}
+        </th>
       ),
     };
   });
 
-  return <Table {...props} config={updateConfig} />;
+  let sortedData = data;
+  if (sortOrder && sortBy) {
+    const { sortValue } = config.find((column) => column.label === sortBy);
+    sortedData = [...data].sort((a, b) => {
+      const aVal = sortValue(a);
+      const bVal = sortValue(b);
+      const reverseOrder = sortOrder === 'asc' ? 1 : -1;
+      if (typeof aVal === 'string') {
+        return aVal.localeCompare(bVal) * reverseOrder;
+      }
+      return (aVal - bVal) * reverseOrder;
+    });
+  }
+  return <Table {...props} data={sortedData} config={updateConfig} />;
 };
 
 export default Table;
+
+const getIcon = (label, sortBy, sortOrder) => {
+  if (label !== sortBy) {
+    return 'V A';
+  }
+  if (sortOrder === null) {
+    return 'V A';
+  } else if (sortOrder === 'asc') {
+    return 'V';
+  } else if (sortOrder === 'desc') {
+    return 'A';
+  }
+};
